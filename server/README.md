a3bfd4c917ede9d0130ac41e5b5dbb4b04c983d3
Brian Evans <ebrian101@gmail.com>
2022-01-22 13:58:39 +0000

    Added backend README

---
+++ server/README.md
@@ -0,0 +1,10 @@
+# Web server for Crypto Impact
+
+To run the web server in development mode, run `npm run dev` in the `server` directory.
+
+
+To build server for production, run 
+```powershell
+npm run build
+npm run start
+```
--- server/webserver.ts
+++ server/webserver.ts
@@ -4,11 +4,14 @@ import express from 'express'
 const app = express()
 
 
+app.get('/', (req, res)=>{
+  res.status(200).send('Crypto Impact API')
+})
+
 app.get('calculateTransactionCost', (req, res)=>{
-
+  const { walletAddress } = req.query // url will be like /calculateTransactionCost?walletAddress=alsdkfj
   // work out energy cost of wallet address
-
   res.status(200)
 })
 
-app.listen(5000, ()=>console.log('http://localhost:5000'))
+app.listen(5000, ()=>console.log('API web server listening on http://localhost:5000'))
