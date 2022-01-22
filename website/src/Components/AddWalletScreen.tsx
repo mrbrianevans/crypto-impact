1ea60623ba973742d400af694e15a0a971795811
Brian Evans <ebrian101@gmail.com>
2022-01-22 20:25:07 +0000

    Updated UI to use new API response interface

--- website/src/Components/AddWalletScreen.tsx
+++ website/src/Components/AddWalletScreen.tsx
@@ -1,18 +1,19 @@
 import React, {useState} from 'react';
 import styles from './AddWalletScreen.module.css'
-import {Button, Card, Divider, Empty, Input, Space} from 'antd';
+import {Badge, Button, Card, Divider, Empty, Input, List, Space, Statistic} from 'antd';
 import Title from "antd/es/typography/Title";
+import {ImpactResponse} from "../../../common/types/ImpactResponse";
 
 const {Search} = Input;
 type AddWalletScreenProps = {}
 const AddWalletScreen: React.FC<AddWalletScreenProps> = (props) => {
-  const [apiResponse, setApiResponse] = useState<string | null>(null)
+  const [apiResponse, setApiResponse] = useState<ImpactResponse | null>(null)
   const [loading, setLoading] = useState(false)
 
   async function callApi(walletAddress: string) {
     setLoading(true)
     const res = await fetch('http://localhost:5000/calculateTransactionCost?' + new URLSearchParams({walletAddress}))
-      .then(r => r.text())
+      .then(r => r.json())
       .finally(() => setLoading(false))
 
     console.log('API response', res)
@@ -29,29 +30,30 @@ const AddWalletScreen: React.FC<AddWalletScreenProps> = (props) => {
       {apiResponse && <Space direction={'vertical'}>
           <Space direction={'horizontal'} align={'center'}>
               <Card title={'Impact'} style={{width: 300}}>
-        <pre>
-            {JSON.stringify(apiResponse, null, 2)}
-        </pre>
+                  <Statistic title={'Energy consumption (KWh)'} value={apiResponse.totalCostKwh}/>
               </Card>
               <Card title={'Impact'} style={{width: 300}}>
-        <pre>
-            {JSON.stringify(apiResponse, null, 2)}
-        </pre>
+                  <Statistic title={'Total transactions'} value={apiResponse.totalCostTxs}/>
               </Card>
-              <Card title={'Impact'} style={{width: 300}}>
-        <pre>
-            {JSON.stringify(apiResponse, null, 2)}
-        </pre>
-              </Card>
-
           </Space>
-          <Card title={'Impact'}>
-        <pre>
-      {JSON.stringify(apiResponse, null, 2)}
-        </pre>
+
+          <Card title={'Cost breakdown - transactions'}>
+              <List bordered
+                    dataSource={apiResponse.costBreakDown}
+                    renderItem={transaction => (<Badge.Ribbon text={transaction.transaction.txid}>
+                      <List.Item>{transaction.relativeImpactKwh} KWh</List.Item>
+                    </Badge.Ribbon>)}
+              />
           </Card>
 
-          <Button danger onClick={()=>setApiResponse(null)}>Clear</Button>
+
+          <Card title={'Full API Response'}>
+            <pre>
+              {JSON.stringify(apiResponse, null, 2)}
+            </pre>
+          </Card>
+
+          <Button danger onClick={() => setApiResponse(null)}>Clear</Button>
       </Space>
       }
 
