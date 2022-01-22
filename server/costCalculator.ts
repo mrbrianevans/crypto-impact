f4f84cd69b59c2cb20685b89fbe69369172a2fd6
zs351 <zs351@cam.ac.uk>
2022-01-22 16:14:16 +0000

    implemented a proportionLimit

--- server/costCalculator.ts
+++ server/costCalculator.ts
@@ -7,32 +7,32 @@ import {TransactionWithStats} from "common/types/BlockchainStats";
  * @param address
  */
 const depthLimit = 10;
-export async function findEnergyCost(address: Address, depth: number): Promise<number> {
+const prLimit = 0.1;
+export async function findEnergyCost(address: Address, depth: number, costPC: number = 1): Promise<number> {
     const transactions = await getTransactionsWithStats(address);
-
     let totalCost = 0;
-    if(depth>depthLimit){
+    if(depth>depthLimit || costPC < prLimit){
         return 0;
     }else {
         for (const transaction of transactions.slice(0,50)) {
-            const txCost = findTransactionCost(transaction, depth);
+            const txCost = findTransactionCost(transaction, depth, costPC);
             totalCost += await txCost;
         }
         return totalCost;
     }
 }
 
-async function findTransactionCost(transaction: TransactionWithStats, depth: number): Promise<number> {
+async function findTransactionCost(transaction: TransactionWithStats, depth: number, costPC:number): Promise<number> {
 
     let energyCost = 1;
     let costProportion = transaction.received / transaction.totalSent;
+    costPC *= costProportion;
     let energyRate = getEnergyRate(transaction.date);
     let energyPerTransaction = getEnergyPerTransaction(energyRate, transaction.block);
-    return (energyPerTransaction * energyCost) + costProportion * await findEnergyCost(transaction.sender[0], depth + 1);
+    return (energyPerTransaction * energyCost) + costProportion * await findEnergyCost(transaction.sender[0], depth + 1, costPC);
 
 }
 
 function getEnergyPerTransaction(energyRate: number, block: Block): number{
     return energyRate*getBlockSolvingTime(block)/block.n_tx;
 }
-
