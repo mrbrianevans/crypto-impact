9029d72089bcdf5bd79bbdf278a29f7bb18ac5b2
Brian Evans <ebrian101@gmail.com>
2022-01-22 18:34:03 +0000

    Added CORS to webserver for localhost:3000

--- server/webserver.ts
+++ server/webserver.ts
@@ -1,10 +1,14 @@
 import express from 'express'
+import cors from 'cors'
 import {findEnergyCost} from "./costCalculator";
 import {getAddress} from "./costApis";
 
 
 const app = express()
-
+const corsOptions = {
+  origin: 'http://localhost:3000'
+}
+app.use(cors(corsOptions))
 
 app.get('/', (req, res)=>{
   res.status(200).send('Crypto Impact API')
@@ -12,6 +16,7 @@ app.get('/', (req, res)=>{
 
 app.get('/calculateTransactionCost',async (req, res)=>{
   const { walletAddress } = req.query // url will be like /calculateTransactionCost?walletAddress=alsdkfj
+  console.log('Request with', { walletAddress })
   // work out energy cost of wallet address
   const energyCost = await findEnergyCost(await getAddress(walletAddress as string), 1);
   res.status(200).send(`${energyCost.toFixed(3)} kWh`);
