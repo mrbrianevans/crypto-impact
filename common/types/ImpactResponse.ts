43d60ac1576517474ae78df0fa7ed7c3fe40e493
Walid Nawfal Sabihi <walid_sabihi@hotmail.com>
2022-01-22 19:27:02 +0000

    Comment TransactionCost interface to explain fields

--- common/types/ImpactResponse.ts
+++ common/types/ImpactResponse.ts
@@ -8,10 +8,18 @@ export interface ImpactResponse {
 }
 
 export interface TransactionCost {
+    // Wrap transaction as part of the cost object
     transaction: Transaction;
 
+    // Total impact in tx units without the cost proportionality weights applied
+    // This will be 1 by default or whatever the default cost unit is
     impactTxs: number;
+
+    // Total impact in tx units with the cost proportionality weights applied
+    // This is the same calculation as relativeImpactKwh but without the tx unit -> energy rate calculation
     relativeImpactTxs: number;
 
+    // Total impact in kwh with the cost proportionality weights applied
+    // This is the main indicator and the default calculation to make
     relativeImpactKwh: number;
 }
